d30a740952eaac3e3e41622f232f2229f72c9384
Viktor Trojan <viktor.trojann@gmail.com>
2024-06-27 17:04:37 +0200

    reading anvil region successfully

--- MultipotJS/src/services/Minecraft.ts
+++ MultipotJS/src/services/Minecraft.ts
@@ -1,66 +1,93 @@
 import Logger from "@/util/Logger.js";
 import { Service } from "./Service.js";
+import * as fs from 'fs';
 
 import MinecraftData from "minecraft-data";
 import minecraftProtocol from 'minecraft-protocol';
 import prismarineChunkLoader from 'prismarine-chunk';
 import AnvilProvider from 'prismarine-provider-anvil';
+import PrismarineWorld from 'prismarine-world';
 import { Vec3 } from "vec3";
+import { Schematic } from 'prismarine-schematic'
+
+
 export class Minecraft extends Service {
     constructor({ enabled = false, reportip = false, discord_webhook }) {
         super({ service_name: 'minecraft', port: 25565, enabled, reportip, discord_webhook });
     }
 
+    generateSimpleChunk(chunkX, chunkZ) {
+        const Chunk = prismarineChunkLoader('1.16');
+        const chunk = new Chunk()
+
+        for (let x = 0; x < 16; x++) {
+            for (let z = 0; z < 16; z++) {
+                chunk.setBlockType(new Vec3(x, 50, z), 1)
+                for (let y = 0; y < 256; y++) {
+                    chunk.setSkyLight(new Vec3(x, y, z), 15)
+                }
+            }
+        }
+
+        return chunk
+    }
+
     async start() {
-        const server: any = minecraftProtocol.createServer({
-            motd: 'CraftAttack SMP Server',
+        // Create a simple Minecraft server
+        const server = minecraftProtocol.createServer({
             'online-mode': true,
-            port: this.port,
-            keepAlive: true,
+            port: 25565, // optional
             version: '1.16',
         });
 
         const mcData = MinecraftData(server.version)
         const loginPacket = mcData.loginPacket
 
-        const Anvil = AnvilProvider.Anvil('1.16');
-        const anvil = new Anvil('C:\\Users\\Anix\\AppData\\Roaming\\.minecraft\\saves\\Nothing\\region')
+        server.on('login', async (client) => {
+            const Anvil = AnvilProvider.Anvil('1.16');
+            const World = PrismarineWorld('1.16');
+            const anvil = new Anvil('C:\\Users\\Anix\\AppData\\Roaming\\.minecraft\\saves\\N\\region');
+            const world = new World(null, anvil);
+            const x = 0; // Chunk coordinates
+            const z = 0;
 
-        const d = anvil.load(0, 0);
+            // const schematic = await Schematic.read(await fs.readFileSync('other/griefer.schem'))
+            // schematic.paste(world, new Vec3(0, 0, 0));
 
-        // Create a new ChunkColumn instance
-        const Chunk = prismarineChunkLoader('1.16');
-        let chunk = new Chunk({
-            minY: 1,
-            worldHeight: 128
-        })
+            // Load the chunk
+            let chunk = await world.getColumn(x, z);
 
-        d.then(function (data) { // data of type chunk
-            chunk.load(data.dump());
-            chunk.loadBiomes(data.dumpBiomes());
-
-            for (let x = 0; x < 16; x++) {
-                for (let z = 0; z < 16; z++) {
-                    for (let y = 0; y < 30; y++) {
-                        // block.name
-                        chunk.setBlockType(new Vec3(x, y, z), mcData.blocksByName.grass_block.id)
-                        chunk.setBlockData(new Vec3(x, y, z), 1)
-                    }
-                }
-            }
-        }).catch(function (err) { console.log(err.stack) })
-
-        server.on('playerJoin', function (client) {
+            // Send the chunk to the client
             client.write('login', {
-                ...loginPacket,
+                entityId: client.id,
+                levelType: 'default',
                 gameMode: 1,
-                maxPlayers: 3,
-                viewDistance: 4,
+                previousGameMode: 255,
+                worldNames: ['minecraft:overworld'],
+                dimensionCodec: loginPacket.dimensionCodec,
+                dimension: 'minecraft:overworld',
+                worldName: 'minecraft:overworld',
+                hashedSeed: [0, 0],
+                maxPlayers: server.maxPlayers,
+                viewDistance: 10,
+                reducedDebugInfo: false,
                 enableRespawnScreen: true,
-            })
-            client.write('map_chunk', {
+                isDebug: false,
+                isFlat: false
+            });
+
+            client.write('position', {
                 x: 0,
+                y: 100,
                 z: 0,
+                yaw: 0,
+                pitch: 0,
+                flags: 0x00
+            });
+
+            client.write('map_chunk', {
+                x,
+                z,
                 groundUp: true,
                 biomes: chunk.dumpBiomes !== undefined ? chunk.dumpBiomes() : undefined,
                 heightmaps: {
@@ -71,16 +98,8 @@ export class Minecraft extends Service {
                 bitMap: chunk.getMask(),
                 chunkData: chunk.dump(),
                 blockEntities: []
-            })
-            client.write('position', {
-                x: 7,
-                y: 30,
-                z: 7,
-                yaw: 137,
-                pitch: 0,
-                flags: 0x00
-            })
-        })
+            });
+        });
 
         server.on('listening', () => {
             Logger.log(`Listening Minecraft on Port ${this.port}`);

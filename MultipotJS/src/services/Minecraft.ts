e9bebc3ea7ee96b669fcc204088beca728d051af
Viktor Trojan <viktor.trojann@gmail.com>
2024-06-27 19:07:17 +0200

    working chunk, sign fix and respawn required

--- MultipotJS/src/services/Minecraft.ts
+++ MultipotJS/src/services/Minecraft.ts
@@ -60,9 +60,19 @@ export class Minecraft extends Service {
 
         // chunk.load(chunk2.dump(), 25, true);
         chunk.load(await fs.readFileSync(__dirname + '/../../other/chunk.dump'), 25, true);
+
+
+        for (let x = 0; x < 16; x++) {
+            for (let z = 0; z < 16; z++) {
+                for (let y = 0; y < 52; y++) {
+                chunk.setBlockType(new Vec3(x, 50, z), 0)
+                }
+            }
+        }
+
         // const out = chunk.dump();
-        // console.log(chunk2.getMask())
-        // fs.writeFileSync(__dirname + '/../../other/chunk.mask', out);
+        // // console.log(chunk2.getMask())
+        // fs.writeFileSync(__dirname + '/../../other/chunk2.mask', out);
 
         // const schematic = await Schematic.read(await fs.readFileSync(__dirname + '/../../other/grief.schem'))
         // // schematic.forEach((block, pos) => {
